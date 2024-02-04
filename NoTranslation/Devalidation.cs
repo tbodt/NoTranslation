f58df982cb60068cc0043c46c1b70a6b86f094e8
tbodt <tblodt@icloud.com>
2024-02-03 16:10:23 -0800

    Simplify code, add devalidation mod

---
+++ NoTranslation/Devalidation.cs
@@ -0,0 +1,101 @@
+﻿using COSML;
+using COSML.Modding;
+using UnityEngine;
+
+namespace NoTranslation
+{
+    public class Devalidation : Mod, IModTogglable
+    {
+        public Devalidation() : base("Devalidation") { }
+
+        public override void Init()
+        {
+            On.JournalKeyUI.CheckClick += JournalKeyUI_CheckClick;
+            On.JournalSlotBoxUI.CheckClick += JournalSlotBoxUI_CheckClick;
+            On.JournalKeyUI.UnassignKey += JournalKeyUI_UnassignKey;
+            On.JournalSlotUI.ChangeState += JournalSlotUI_ChangeState;
+            On.JournalKeyUI.GetCursorType += JournalKeyUI_GetCursorType;
+            On.JournalSlotBoxUI.GetCursorType += JournalSlotBoxUI_GetCursorType;
+        }
+
+        public void Unload()
+        {
+            On.JournalKeyUI.CheckClick -= JournalKeyUI_CheckClick;
+            On.JournalSlotBoxUI.CheckClick -= JournalSlotBoxUI_CheckClick;
+            On.JournalKeyUI.UnassignKey -= JournalKeyUI_UnassignKey;
+            On.JournalSlotUI.ChangeState -= JournalSlotUI_ChangeState;
+        }
+
+        private void JournalKeyUI_CheckClick(On.JournalKeyUI.orig_CheckClick orig, JournalKeyUI self)
+        {
+            InputsController inputsController = GameController.GetInstance().GetInputsController();
+            if (inputsController.OnStartDrag())
+            {
+                inputsController.EnterDragUI(self);
+            }
+            else
+            {
+                orig(self);
+            }
+        }
+
+        private void JournalSlotBoxUI_CheckClick(On.JournalSlotBoxUI.orig_CheckClick orig, JournalSlotBoxUI self)
+        {
+            InputsController inputsController = GameController.GetInstance().GetInputsController();
+            if (inputsController.OnStartDrag())
+            {
+                inputsController.EnterDragUI(self);
+            }
+            else
+            {
+                orig(self);
+            }
+        }
+
+        private static string AnimatorCurrentClipName(Animator animator)
+        {
+            AnimatorClipInfo[] current = animator.GetCurrentAnimatorClipInfo(0);
+            return current.Length == 0 ? null : current[0].clip.name;
+        }
+
+        private void JournalKeyUI_UnassignKey(On.JournalKeyUI.orig_UnassignKey orig, JournalKeyUI self)
+        {
+            Animator animator = ReflectionHelper.GetField<JournalKeyUI, Animator>(self, "animator");
+            orig(self);
+            if (AnimatorCurrentClipName(animator) == "JournalKeyValidatedAnimation")
+            {
+                animator.Play("Base Layer.Init");
+            }
+        }
+
+        private void JournalSlotUI_ChangeState(On.JournalSlotUI.orig_ChangeState orig, JournalSlotUI self, int newState)
+        {
+            Animator animator = ReflectionHelper.GetField<JournalSlotUI, Animator>(self, "slotAnimator");
+            orig(self, newState);
+            if (AnimatorCurrentClipName(animator) == "JournalSlotValidAnimation")
+            {
+                animator.Play("Base Layer.Init");
+            }
+        }
+
+        private InteractiveCursorType JournalSlotBoxUI_GetCursorType(On.JournalSlotBoxUI.orig_GetCursorType orig, JournalSlotBoxUI self)
+        {
+            if (self.GetSlotUI().GetRuneData() != null && !GameController.GetInstance().GetJournal().WaitValidAnimation())
+            {
+                return InteractiveCursorType.DRAG;
+            }
+            return orig(self);
+        }
+
+        private InteractiveCursorType JournalKeyUI_GetCursorType(On.JournalKeyUI.orig_GetCursorType orig, JournalKeyUI self)
+        {
+            if (self.GetRuneData().GetState() == RuneStateData.VALIDATED)
+            {
+                return InteractiveCursorType.BUTTON;
+            }
+            return orig(self);
+        }
+
+    }
+}
+
--- NoTranslation/NoTranslation.cs
+++ NoTranslation/NoTranslation.cs
@@ -1,26 +1,13 @@
 ﻿using COSML.Modding;
-using System.Collections.Generic;
-using static COSML.Menu.MenuUtils;
-using System;
-using COSML;
 
 namespace NoTranslation
 {
-    public class NoTranslation : Mod, ILocalSettings<LocalData>, IModTogglable, IModMenu
+    public class NoTranslation : Mod, IModTogglable
     {
-        private GlobalData globalData = new GlobalData();
-        private LocalData localData = new LocalData();
-
         public NoTranslation() : base("No Translation") { }
-        public override string GetVersion() => "1.0.0";
-        public void OnLoadGlobal(GlobalData data) => globalData = data;
-        public GlobalData OnSaveGlobal() => globalData;
-        public void OnLoadLocal(LocalData data) => localData = data;
-        public LocalData OnSaveLocal() => localData;
 
         public override void Init()
         {
-            Info("Loaded NoTranslation");
             On.JournalLanguageUI.JournalPage.TryValidPage += Hook_JournalPage_TryValidPage;
             On.PageRevelator.ForceRevelated += Hook_PageRevelator_ForceRevelated;
             On.PageRevelator.GetState += Hook_PageRevelator_GetState;
@@ -30,7 +17,6 @@ namespace NoTranslation
 
         public void Unload()
         {
-            Info("Unloaded NoTranslation");
             On.JournalLanguageUI.JournalPage.TryValidPage -= Hook_JournalPage_TryValidPage;
             On.PageRevelator.ForceRevelated -= Hook_PageRevelator_ForceRevelated;
             On.PageRevelator.GetState -= Hook_PageRevelator_GetState;
@@ -58,12 +44,6 @@ namespace NoTranslation
         {
             return true;
         }
-
-        public List<IOptionData> GetMenu()
-        {
-            return new List<IOptionData>
-            {
-            };
-        }
     }
+
 }
